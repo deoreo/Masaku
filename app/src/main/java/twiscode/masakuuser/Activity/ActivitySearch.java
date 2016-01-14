b01a637ead1061bd31b7623443de782e3fb861e4
user <user>
2016-01-14 11:42:33 +0700

    activity search

---
+++ app/src/main/java/twiscode/masakuuser/Activity/ActivitySearch.java
@@ -0,0 +1,23 @@
+package twiscode.masakuuser.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import twiscode.masakuuser.R;
+
+/**
+ * Created by TwisCode-02 on 10/26/2015.
+ */
+public class ActivitySearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+
+
+
+    }
+
+}
